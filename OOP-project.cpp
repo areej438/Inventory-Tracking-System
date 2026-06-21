b41163dbfca99edd4c15742d028243fcb065b301
areej438 <areejmehmood90@gmail.com>
2026-06-21 16:01:15 +0500

    Add project Entry Point

---
+++ OOP-project.cpp
@@ -0,0 +1,21 @@
+// OOP-project.cpp, This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include "welcomeBaner.h"
+#include "mainMenu.h"
+using namespace std;
+
+
+int main()
+{
+	loadingMsg();
+	welcomeBanner();
+	beforeMainmenuMsg();
+	welcomeBanner();
+	mainMenu();
+		// for checking
+	//product p1;
+	//product p2;
+	//salesOrder so1;
+	//stockItem si1;
+}
